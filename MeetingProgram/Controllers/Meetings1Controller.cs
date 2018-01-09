11d274fb47ea9280d749fbc1c29bae3fecc4e224
Groovesmith <jglad16@student.aau.dk>
2018-01-09 21:41:39 +0100

    ff

---
+++ MeetingProgram/Controllers/Meetings1Controller.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MeetingProgram.Data;
+using MeetingProgram.Models.MeetingModel;
+
+namespace MeetingProgram.Controllers
+{
+    public class Meetings1Controller : Controller
+    {
+        private MeetingProgramContext db = new MeetingProgramContext();
+
+        // GET: Meetings1
+        public ActionResult Index()
+        {
+            return View(db.Meetings.ToList());
+        }
+
+        // GET: Meetings1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Meeting meeting = db.Meetings.Find(id);
+            if (meeting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(meeting);
+        }
+
+        // GET: Meetings1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Meetings1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MeetingID,Date,Description,IsDraft")] Meeting meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Meetings.Add(meeting);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(meeting);
+        }
+
+        // GET: Meetings1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Meeting meeting = db.Meetings.Find(id);
+            if (meeting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(meeting);
+        }
+
+        // POST: Meetings1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MeetingID,Date,Description,IsDraft")] Meeting meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(meeting).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(meeting);
+        }
+
+        // GET: Meetings1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Meeting meeting = db.Meetings.Find(id);
+            if (meeting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(meeting);
+        }
+
+        // POST: Meetings1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Meeting meeting = db.Meetings.Find(id);
+            db.Meetings.Remove(meeting);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
